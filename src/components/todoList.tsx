534b852003540674d3e7e07f2e7a5b552459c6f4
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-06 18:23:39 +0100

    add a time to my todo's

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -6,24 +6,37 @@ interface Props {
 }
 
 const TodoList = ({ todos, onDeleteTodo }: Props) => {
+const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    };
+
+    return new Intl.DateTimeFormat("EU", options).format(new Date(dateString));
+};
+
   return (
     <div>
       <h1>Todo List</h1>
-      <ul className="list-group">
+      <h2>All my todo's</h2>
+    <ul className="list-group">
         {todos.map((todo) => (
-          <li className="list-group-item" key={todo._id}>
-            {todo.title}
-            <button
-              onClick={() => onDeleteTodo(todo._id)}
-              className="btn btn-danger"
-              
-            >
-             Delete
-            </button>
-
-          </li>
+            <li className="list-group-item" key={todo._id}>
+                {todo.title}
+                {todo.date && <p>{formatDate(todo.date.toString())}</p>}
+                <button
+                    onClick={() => onDeleteTodo(todo._id)}
+                    className="btn btn-danger"
+                >
+                    Delete this todo
+                </button>
+            </li>
         ))}
-      </ul>
+    </ul>
     </div>
   );
 };
