de257d41d8deb382cf8ebf92b4823f2b6f59545d
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-06 12:12:27 +0100

    try again

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
-import api_service from './services/api_service'
+//import api_service from './services/api_service'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
   
   //Get the todo API services here
   return (
